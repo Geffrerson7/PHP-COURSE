f3464d6eeab56b15f1808dd71c17ccfdf26c1c33
Gefferson Casasola <gcasasolah@uni.pe>
2023-06-24 23:37:31 -0500

    Add exercise 36 and 37

---
+++ ejercicio46.php
@@ -0,0 +1,14 @@
+<!-- Funcion include_once -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funcion include_once</title>
+</head>
+<body>
+<?php include_once "ejercicio46_1.php"; ?>
+<?php include_once "ejercicio46_1.php"; ?>
+<?php echo "Hola, estoy en la página principal"; ?>
+</body>
+</html>
