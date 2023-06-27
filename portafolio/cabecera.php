d1e531d687daa1f11d667558f7a5c44064b6fbc9
Gefferson Casasola <gcasasolah@uni.pe>
2023-06-26 23:58:21 -0500

    Add header

---
+++ portafolio/cabecera.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Mi portafolio</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+
+<body>
+    <a href="index.php">Inicio</a> |
+    <a href="portafolio">Portafolio</a> |
+    <a href="cerrar.php">Cerrar</a> |
+    <br />
